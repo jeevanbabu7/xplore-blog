8f81e6b331dbf6a703a4e0e4722152c7acd725c2
jeevanbabu7 <jeevanbabu2710@gmail.com>
2025-02-08 13:35:44 +0530

    some change

--- client/src/components/Youtube.jsx
+++ client/src/components/Youtube.jsx
@@ -7,7 +7,7 @@ const Youtube = ({ url }) => {
 
       <div className="relative w-full aspect-video">
         <iframe
-          src="https://player.twitch.tv/?channel=aatmikrobocek&parent=xplore-blog-seven.vercel.app/"
+          src="https://player.twitch.tv/?channel=aatmikrobocek&parent=xplore-blog-seven.vercel.app"
           frameBorder="0"
           allowFullScreen
           scrolling="no"
